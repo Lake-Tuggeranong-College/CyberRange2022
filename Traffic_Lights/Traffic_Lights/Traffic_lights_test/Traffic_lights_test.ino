17f2a4b02b55efb37723f2791d7a07863ad40994
Jackson <0706262@schoolsnet.act.edu.au>
2022-02-16 11:37:30 +1030

    Update Traffic_lights_test.ino
    Implemented the pin definitions for the traffic lights

--- Traffic_Lights/Traffic_Lights/Traffic_lights_test/Traffic_lights_test.ino
+++ Traffic_Lights/Traffic_Lights/Traffic_lights_test/Traffic_lights_test.ino
@@ -14,7 +14,9 @@
 #define RED_LIGHT 2
 
 // Traffic light definitions
-const int ledPin = 13; // the number of the red LED pin
+const int Light_1_RED = 13; // the number of the red LED pin
+const int Light_1_YELLOW = 14; // the number of the Yellow LED pin
+const int Light_1_GREEN = 15; // the number of the GREEN LED pin
 
 void setup() {
   // Traffic light serial setup
