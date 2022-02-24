40ebb9b771164cc23433763aa5b57e969371c944
abhishekwick <7064470@schoolsnet.act.edu.au>
2022-02-25 09:00:47 +1100

    Update Test_code_for_piezo.ino

--- Emergencyservices/Emergencyservices/Test_code_for_piezo.ino
+++ Emergencyservices/Emergencyservices/Test_code_for_piezo.ino
@@ -1,15 +1,1 @@
-int speakerPin = 8; // control horn pin
-int potPin = 4; // control pin adjustable resistor
-int value = 0;
 
-void setup();
-pinMode(speakerPin, OUTPUT);
-}
-
-void loop() {
-value = analogRead(potPin); // reading resistor values pin
-digitalWrite(speakerPin, HIGH);
-delay(value); // adjust the speaker sound of the time;
-digitalWrite(speakerPin, LOW);
-delay(value); // adjust the speaker does not ring a time;
-}
