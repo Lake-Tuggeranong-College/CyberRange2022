9b80efb8b3387d408f61ffe755d16d613ee6d496
abhishekwick <0706493@schoolsnet.act.edu.au>
2022-02-25 09:46:38 +1100

    working emergency services code

--- Emergencyservices/Emergencyservices/Emergencyservices.ino
+++ Emergencyservices/Emergencyservices/Emergencyservices.ino
@@ -52,7 +52,7 @@ char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursd
 
 const int LOOPDELAY = 25;
 
-void setup();
+void setup() {
   Serial.begin(9600);
   while (!Serial) {
     delay(10);
@@ -76,12 +76,12 @@ void setup();
   Serial.print("Connected to the Internet");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  
+
 
 
 
   routesConfiguration(); // Reads routes from routesManagement
-  
+
   server.begin();
 
 
@@ -110,7 +110,7 @@ void loop() {
 }
 
 void emergencyservices() {
-  //emergency services will randomly mnake static noises 
+  //emergency services will randomly mnake static noises
 }
 
 void logEvent(String dataToLog) {
