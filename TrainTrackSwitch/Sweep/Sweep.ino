fdc81e53d8098c549f2c1df348629f9733135333
krishna <0186359@schoolsnet.act.edu.au>
2022-03-21 14:16:07 +1100

    Updated dahboard

--- TrainTrackSwitch/Sweep/Sweep.ino
+++ TrainTrackSwitch/Sweep/Sweep.ino
@@ -66,6 +66,8 @@ void setup() {
 void loop() {
 myservo.write(180);
 delay(1000);
+myservo.write(0);
+delay(1000);
 //	for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
 //		// in steps of 1 degree
 //		myservo.write(pos);    // tell servo to go to position in variable 'pos'
