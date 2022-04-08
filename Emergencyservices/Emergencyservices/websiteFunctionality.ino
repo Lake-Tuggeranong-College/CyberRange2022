cf769808b93b3c189bc4fbe5edb510b3a083ea7a
Matthew Seebohm <0706493@schoolsnet.act.edu.au>
2022-04-08 10:22:27 +1000

    Added function headers

--- Emergencyservices/Emergencyservices/websiteFunctionality.ino
+++ Emergencyservices/Emergencyservices/websiteFunctionality.ino
@@ -27,6 +27,7 @@ void routesConfiguration() {
 
   // Example of route with authentication, and use of processor
   // Also demonstrates how to have arduino functionality included (turn LED on)
+  //turns siren on 
   server.on("/Sirenon", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
@@ -35,8 +36,7 @@ void routesConfiguration() {
     request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
   });
 
-// when someone presses siren on the speaker will start making the beeping noise.
-
+//turns siren off 
 server.on("/Sirenoff", HTTP_GET, [](AsyncWebServerRequest * request) {
     if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
@@ -48,13 +48,14 @@ server.on("/Sirenoff", HTTP_GET, [](AsyncWebServerRequest * request) {
   // when someone presses siren off the speaker will stop making the beeping noise.
   
   // Example of route which sets file to download - 'true' in send() command.
+  //downloads logevent
   server.on("/logOutput", HTTP_GET, [](AsyncWebServerRequest * request) {
     logEvent("output");
     request->send(SPIFFS, "/logEvents.csv", "text/html", true);
   });
 }
 
-
+//Date and time code
 String getDateTime() {
    DateTime rightNow = rtc.now();
   char csvReadableDate[25];
