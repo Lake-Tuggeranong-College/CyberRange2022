68d8c937f5905a548708d20a7328c7c75342c70d
LTC-IT <ryan.cather@ed.act.edu.au>
2022-05-05 21:37:59 +1000

    Added OS check so system doesn't crash when testing on non-RPI

--- Website/bp_missile_launcher.py
+++ Website/bp_missile_launcher.py
@@ -1,9 +1,8 @@
 # All the code/routes for the missile launcher will be stored here.
 
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template
 from flask_login import current_user, login_required
-from models import User
-from forms import MissileLauncherLoginForm
+import os
 
 missile_launcher_blueprint = Blueprint('missile_launcher_blueprint', __name__)
 
@@ -40,14 +39,19 @@ def fire():
 def fire_missile():  # This is the fire function controls the missile fire
     """This function is what allows the missile to fire,
     it also detects the missile launcher from the USB."""
-    dev = usb.core.find(idVendor=0x2123, idProduct=0x1010)  # Finds the missile launcher via usb.
-    if dev is None:  # If it can't find the missile launcher.
-        raise ValueError('Launcher not found.')  # This will be printed if the missile launcher cannot be found.
-    # Determines whether there is a Kernel driver associated with the missile launcher.
-    if dev.is_kernel_driver_active(0) is True:
-        dev.detach_kernel_driver(0)  # If there is a Kernal driver associated with the missile launcher it detaches it.
-        dev.set_configuration()  # The code will then configure the USB missile launcher.
 
-    # This is what allows the missile to fire.
-    dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-    return render_template('missile_launcher/fired.html')
+    if os.uname()[4][:3] == 'arm':
+        dev = usb.core.find(idVendor=0x2123, idProduct=0x1010)  # Finds the missile launcher via usb.
+        if dev is None:  # If it can't find the missile launcher.
+            raise ValueError('Launcher not found.')  # This will be printed if the missile launcher cannot be found.
+        # Determines whether there is a Kernel driver associated with the missile launcher.
+        if dev.is_kernel_driver_active(0) is True:
+            dev.detach_kernel_driver(0)  # If there is a Kernal driver associated with the missile launcher it detaches it.
+            dev.set_configuration()  # The code will then configure the USB missile launcher.
+
+        # This is what allows the missile to fire.
+        dev.ctrl_transfer(0x21, 0x09, 0, 0, [0x02, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        return render_template('missile_launcher/fired.html', output="Fired", user=current_user)
+
+    else:
+        return render_template('missile_launcher/fired.html', output="This functionality only works on a raspberry pi", user=current_user)
